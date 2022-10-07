76973637a5b95fbc60c886f09ab4e92f70e0df43
ldimbe20 <ldimbe20@gmail.com>
2022-10-07 11:02:50 -0500

    User form is working and profiles are displayed

--- app/routes/new.tsx
+++ app/routes/new.tsx
@@ -1,58 +1,59 @@
-// import type { ActionFunction } from "@remix-run/node";
-// import { redirect } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 
-// import { db } from "~/utils/db.server";
+import { db } from "~/utils/db.server";
+import { requireUserId } from "~/utils/session.server";
 
-// export const action: ActionFunction = async ({
-//   request,
-// }) => {
-//   // const user = loggedInUser
-//   const form = await request.formData();
-//   const name = form.get("name");
-//   const notes = form.get("notes")
-//   const personalAllergy = form.get("personalAllergy");
-//   if (
-//     typeof name !== "string" ||
-//     typeof notes !== "string" || 
-//     typeof personalAllergy !== "string" 
-//   ) {
-//     throw new Error(`Form not submitted correctly.`);
-//   }
+export const action: ActionFunction = async ({
+  request,
+}) => {
+  const userId = await requireUserId(request)
+  const form = await request.formData();
+  const name = form.get("name")
+  const notes = form.get("notes")
+  const personalAllergy = form.get("personalAllergy");
+  if (
+    typeof name !== "string" ||
+    typeof notes !== "string" || 
+    typeof personalAllergy !== "string" 
+  ) {
+    throw new Error(`Form not submitted correctly.`);
+  }
 
-//   const fields = { name, notes, personalAllergy };
+  const fields = { name, notes, personalAllergy};
 
-//   const profile = await db.profile.create({ data: fields });
-//   return redirect(`/profiles/${profile.id}`);
-// };
+  const profile = await db.profile.create({ data: { ...fields, userId } });
+  return redirect(`/profiles/${profile.id}`);
+};
 
 
 
-// export default function NewProfileRoute() {
-//   return (
-//     <div>
-//       <p>Add your own hilarious profile</p>
-//       <form method="post">
-//         <div>
-//           <label>
-//             Name: <input type="text" name="name" />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             Personal Allergy: <textarea name="personalAllergy" />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             Notes: <textarea name="notes" />
-//           </label>
-//         </div>
-//         <div>
-//           <button type="submit" className="button">
-//             Add
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// }
+export default function NewProfileRoute() {
+  return (
+    <div className = "container">
+      <h4>Add friends profile</h4>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Allergy: <input type="text" name="personalAllergy" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Notes: <textarea name="notes" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
