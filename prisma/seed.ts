77f959462a5e9e784bce33b467b9a6a43ca7e634
ldimbe20 <ldimbe20@gmail.com>
2022-10-14 12:00:27 -0500

    Data is now working

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,18 +2,24 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function seed() {
-  const kody = await db.user.create({
-    data: {
-      username: "kody",
-      // this is a hashed version of "twixrox"
-      passwordHash:
-        "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
-    },
-  });
+  // const kody = await db.user.create({
+  //   data: {
+  //     username: "kody",
+  //     // this is a hashed version of "twixrox"
+  //     passwordHash:
+  //       "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+  //   },
+  // });
+  // await Promise.all(
+  //   getProfiles().map((profile) => {
+  //     const data = { userId: kody.id, ...profile };
+  //     return db.profile.create({ data });
+  //   })
+  // );
+
   await Promise.all(
-    getProfiles().map((profile) => {
-      const data = { userId: kody.id, ...profile };
-      return db.profile.create({ data });
+    getFoodRestriction().map((foodRestriction) => {
+      return db.foodRestrictionCategory.create({ data: foodRestriction });
     })
   );
 }
@@ -27,6 +33,21 @@ function getProfiles() {
       name: "Susan",
       personalAllergy: "Milk",
       notes: "He doesn't like eggs"
+    },
+     ];
+}
+
+function getFoodRestriction() {
+
+  return [
+    {
+      name: "Dairy",
+    },
+    {
+      name: "Meat",
+    },
+    {
+      name: "Soy",
     },
      ];
 }
